9c1240db3d6ee6c3c85e4aaae74cb091a88b378d
Varun <varun044@gmail.com>
2019-01-17 04:05:30 +0530

    Improvements.

--- portdata/views.py
+++ portdata/views.py
@@ -12,32 +12,38 @@ def index(request):
 
 
 def upload(request):
-    files = request.FILES.getlist('uploadfiles')
-    for f in files:
-        file_reader = csv.reader(TextIOWrapper(f), delimiter=',')
-        next(file_reader, None)
-        for row in file_reader:
-            obj, created = PortData.objects.get_or_create(
-                product=row[0],
-                quantity=row[1],
-                unit=row[2],
-                item_rate_inv=row[3],
-                currency=row[4],
-                total_amount=row[5],
-                fob_inr=row[6],
-                item_rate_inr=row[7],
-                fob_usd=row[8],
-                foreign_port=row[9],
-                foreign_country=row[10],
-                india_port=row[11],
-                india_company=row[12],
-                foreign_company=row[13],
-                invoice_number=row[14],
-                hs_code=row[15],
-            )
-            print(created)
-    messages.add_message(request, messages.SUCCESS, 'Data imported.')
-    return redirect(request, 'portdata/index.html')
+    try:
+        files = request.FILES.getlist('uploadfiles')
+        for f in files:
+            file_reader = csv.reader(TextIOWrapper(f), delimiter=',')
+            next(file_reader, None)
+            try:
+                for row in file_reader:
+                    obj, created = PortData.objects.get_or_create(
+                        product=row[0],
+                        quantity=row[1],
+                        unit=row[2],
+                        item_rate_inv=row[3],
+                        currency=row[4],
+                        total_amount=row[5],
+                        fob_inr=row[6],
+                        item_rate_inr=row[7],
+                        fob_usd=row[8],
+                        foreign_port=row[9],
+                        foreign_country=row[10],
+                        india_port=row[11],
+                        india_company=row[12],
+                        foreign_company=row[13],
+                        invoice_number=row[14],
+                        hs_code=row[15],
+                    )
+                    print(created)
+            except:
+                messages.add_message(request, messages.ERROR, 'Error importing file ' + f.name + '. Skipping this.')
+        messages.add_message(request, messages.SUCCESS, 'Data imported.')
+    except:
+        messages.add_message(request, messages.ERROR, 'Error importing data.')
+    return redirect('index')
 
 
 class DataViewSet(viewsets.ModelViewSet):
