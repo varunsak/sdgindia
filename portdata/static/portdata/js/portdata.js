692aadaaabed09e8f240be8c04d69aa61f4345c3
Varun <varun044@gmail.com>
2019-01-17 22:15:16 +0530

    UI Improvements.

--- portdata/static/portdata/js/portdata.js
+++ portdata/static/portdata/js/portdata.js
@@ -1,4 +1,9 @@
 $(document).ready( function () {
+    $('.leftmenutrigger').on('click', function(e) {
+     $('.side-nav').toggleClass("open");
+     e.preventDefault();
+    });
+
     $('#datatable tfoot th').each( function () {
         var title = $(this).text();
         $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
