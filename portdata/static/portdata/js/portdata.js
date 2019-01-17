1eb892579b3f1071c42a6baa98580c5ccb9381f1
Varun <varun044@gmail.com>
2019-01-17 21:33:59 +0530

    Column wise filtering added.

--- portdata/static/portdata/js/portdata.js
+++ portdata/static/portdata/js/portdata.js
@@ -1,3 +1,21 @@
 $(document).ready( function () {
-    $('#datatable').DataTable();
+    $('#datatable tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+
+    var table = $('#datatable').DataTable();
+
+    table.columns().every( function () {
+        var that = this;
+
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
 } );
+
